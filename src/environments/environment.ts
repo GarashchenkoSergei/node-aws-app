655c1b1d23ba10190d354f14b0424c2dcaffe642
Sergey Garashchenko <garashchenkosergei@gmail.com>
2022-08-16 09:39:05 +0300

    change lambda endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://kvb8qpslfl.execute-api.eu-west-1.amazonaws.com/dev',
+    import: ' https://9wyj351ifd.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://thmrfeohhf.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
